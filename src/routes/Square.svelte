59482442ef182ed93f2eddddc3dd3fbecc4572f5
Nitesh000 <ntudu040@gmail.com>
2024-01-20 18:26:44 +0530

    feat: added the grid system for easy mode

---
+++ src/routes/Square.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let emoji: string;
+</script>
+
+<div class="square">
+  <button on:click /> 
+   <span>{emoji}</span> 
+</div>
+
+  <style>
+  .square {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: yellow;
+  }
+
+  button {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+
+  span {
+    font-size: 5em;
+  }
+</style>
